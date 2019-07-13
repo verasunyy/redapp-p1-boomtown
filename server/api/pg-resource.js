d8ed887ce186ee885a0b38ad1e4bc94c62d30319
verasunyy <verasun@live.com>
2019-07-12 18:19:34 -0700

    update error message

--- server/api/pg-resource.js
+++ server/api/pg-resource.js
@@ -56,47 +56,23 @@ module.exports = postgres => {
        *  3) If the user is found and there are no errors, return only the id, email, fullname, bio fields.
        *     -- this is important, don't return the password!
        *
-       *  You'll need to complete the query first before attempting this exercise.
        */
 
       const findUserQuery = {
-        //$1 means the first element in the array
-        text: 'SELECT * FROM users WHERE id=$1 LIMIT 1', // @TODO: Basic queries
+        //getting the first element in the array: id=$1
+        text: 'SELECT * FROM users WHERE id=$1 LIMIT 1',
         values: [id]
       };
-
-      /**
-       *  Refactor the following code using the error handling logic described above.
-       *  When you're done here, ensure all of the resource methods in this file
-       *  include a try catch, and throw appropriate errors.
-       *
-       *  Ex: If the user is not found from the DB throw 'User is not found'
-       *  If the password is incorrect throw 'User or Password incorrect'
-       */
       try {
         const user = await postgres.query(findUserQuery);
         return user.rows[0];
-      }
-      catch (e) {
+      } catch (e) {
         throw ("User not found!")
       }
-      // -------------------------------
     },
     async getItems(idToOmit) {
       try {
         const items = await postgres.query({
-          /**
-           *  @TODO:
-           *
-           *  idToOmit = ownerId
-           *
-           *  Get all Items. If the idToOmit parameter has a value,
-           *  the query should only return Items were the ownerid !== idToOmit
-           *
-           *  Hint: You'll need to use a conditional AND and WHERE clause
-           *  to your query text using string interpolation
-           */
-
           text: `SELECT * FROM items WHERE ownerid != $1`,
           values: idToOmit ? [idToOmit] : []
         });
@@ -108,14 +84,9 @@ module.exports = postgres => {
     async getItemsForUser(id) {
       try {
         const items = await postgres.query({
-          /**
-           *  @TODO:
-           *  Get all Items for user using their id
-           */
           text: `SELECT * FROM items WHERE ownerid = $1`,
           values: [id]
         });
-        // console.log(items.rows)
         return items.rows;
       } catch (e) {
         throw ("items not found");
@@ -124,55 +95,37 @@ module.exports = postgres => {
     async getBorrowedItemsForUser(id) {
       try {
         const items = await postgres.query({
-          /**
-           *  @TODO:
-           *  Get all Items borrowed by user using their id
-           */
           text: `SELECT * FROM items WHERE borrowerid=$1`,
           values: [id]
         });
         return items.rows;
-      }
-      catch (e) {
-        throw ("items not found")
+      } catch (e) {
+        throw ("borrowed items not found")
       }
     },
     async getTags() {
-
-      const tags = await postgres.query({
-        text: `SELECT * FROM tags`,
-      });
-      return tags.rows;
+      try {
+        const tags = await postgres.query({
+          text: `SELECT * FROM tags`,
+        });
+        return tags.rows;
+      } catch (e) {
+        throw ("tags not found")
+      }
     },
     async getTagsForItem(id) {
       const tagsQuery = {
         text: `SELECT tags.title FROM tags, item_tags WHERE item_tags.itemid=$1 AND tags.id=item_tags.tagid;`, // @TODO: Advanced query Hint: use INNER JOIN
         values: [id]
       };
-
-      const tags = await postgres.query(tagsQuery);
-      return tags.rows;
+      try {
+        const tags = await postgres.query(tagsQuery);
+        return tags.rows;
+      } catch (e) {
+        throw ("tags for item not found")
+      }
     },
     async saveNewItem({ item, user }) {
-      /**
-       *  @TODO: Adding a New Item
-       *
-       *  Adding a new Item to Posgtres is the most advanced query.
-       *  It requires 3 separate INSERT statements.
-       *
-       *  All of the INSERT statements must:
-       *  1) Proceed in a specific order.
-       *  2) Succeed for the new Item to be considered added
-       *  3) If any of the INSERT queries fail, any successful INSERT
-       *     queries should be 'rolled back' to avoid 'orphan' data in the database.
-       *
-       *  To achieve #3 we'll ue something called a Postgres Transaction!
-       *  The code for the transaction has been provided for you, along with
-       *  helpful comments to help you get started.
-       *
-       *  Read the method and the comments carefully before you begin.
-       */
-
       return new Promise((resolve, reject) => {
         /**
          * Begin transaction by opening a long-lived connection
@@ -186,25 +139,13 @@ module.exports = postgres => {
               const { title, description, tags } = item;
 
               // Generate new Item query
-              // @TODO
               const addItemQuery = {
                 text: `INSERT INTO items (title, description, ownerid) VALUES ($1, $2, 1) RETURNING *`,
                 values: [title, description]
               };
-
               const newItem = await postgres.query(addItemQuery);
-              // console.log(newItem.rows[0].id);
-              // console.log(item);
-              console.log(tags);
 
-              // -------------------------------
-
-              // Insert new Item
-              // @TODO
-              // -------------------------------
-
-              // Generate tag relationships query (use the'tagsQueryString' helper function provided)
-              // @TODO
+              // Generate tag relationships query
               const itemId = newItem.rows[0].id;
               const tagsId = tags.map(tag => tag.id);
 
@@ -213,11 +154,6 @@ module.exports = postgres => {
                 values: tagsId
               };
               const itemTags = await postgres.query(addItemTagsQuery);
-              // -------------------------------
-
-              // Insert tags
-              // @TODO
-              // -------------------------------
 
               // Commit the entire transaction!
               client.query('COMMIT', err => {
@@ -226,9 +162,7 @@ module.exports = postgres => {
                 }
                 // release the client back to the pool
                 done();
-                // Uncomment this resolve statement when you're ready!
                 resolve(newItem.rows[0])
-                // -------------------------------
               });
             });
           } catch (e) {
--- server/api/resolvers/index.js
+++ server/api/resolvers/index.js
@@ -1,18 +1,4 @@
-/**
- *  @TODO: Handling Server Errors
- *
- *  Once you've completed your pg-resource.js methods and handled errors
- *  use the ApolloError constructor to capture and return errors from your resolvers.
- *
- *  Throwing ApolloErrors from your resolvers is a nice pattern to follow and
- *  will help you easily debug problems in your resolving functions.
- *
- *  It will also help you control th error output of your resource methods and use error
- *  messages on the client! (More on that later).
- *
- *  The user resolver has been completed as an example of what you'll need to do.
- *  Finish of the rest of the resolvers when you're ready.
- */
+
 const { ApolloError } = require('apollo-server-express');
 
 // @TODO: Uncomment these lines later when we add auth
@@ -51,130 +37,66 @@ module.exports = app => {
           throw new ApolloError(e);
         }
       },
-      async items(parent, args, { pgResource }, info) {
-        // @TODO: Replace this mock return statement with the correct items from Postgres
-        console.log(args);
+      async items(parent, { filter }, { pgResource }, info) {
         try {
-          const items = await pgResource.getItems(args.filter);
+          const items = await pgResource.getItems(filter);
           return items;
         } catch (e) {
           throw new ApolloError(e);
         }
-        // -------------------------------
       },
-      async tags(parent, { }, { pgResource }, info) {
-        // @TODO: Replace this mock return statement with the correct tags from Postgres
+      async tags(parent, args, { pgResource }, info) {
         try {
           const tags = await pgResource.getTags();
           return tags;
         } catch (e) {
           throw new ApolloError(e);
         }
-        // -------------------------------
       }
     },
 
     User: {
-      /**
-       *  @TODO: Advanced resolvers
-       *
-       *  The User GraphQL type has two fields that are not present in the
-       *  user table in Postgres: items and borrowed.
-       *
-       *  According to our GraphQL schema, these fields should return a list of
-       *  Items (GraphQL type) the user has lent (items) and borrowed (borrowed).
-       *
-       */
-      // @TODO: Uncomment these lines after you define the User type with these fields
-      async items(parent, { id }, { pgResource }, info) {
-        //   // @TODO: Replace this mock return statement with the correct items from Postgres
-        // console.log(parent.id);
+
+      async items({ id }, args, { pgResource }, info) {
         try {
-          const itemsByUser = await pgResource.getItemsForUser(parent.id);
-          // console.log(itemsByUser);
+          const itemsByUser = await pgResource.getItemsForUser(id);
           return itemsByUser;
-        }
-        catch (e) {
+        } catch (e) {
           throw ("items not found");
         }
-        // return pgResource.getItems.filter(item => user.id === item.itemowner);
-        //   // -------------------------------
       },
       async borrowed({ id }, args, { pgResource }, info) {
-        // console.log(parent);
-        //   // @TODO: Replace this mock return statement with the correct items from Postgres
         try {
           return await pgResource.getBorrowedItemsForUser(id);
         } catch (e) {
           throw ("borrowed items not found")
         }
-        //   // -------------------------------
       }
-      // -------------------------------
     },
 
     Item: {
-      /**
-       *  @TODO: Advanced resolvers
-       *
-       *  The Item GraphQL type has two fields that are not present in the
-       *  Items table in Postgres: itemowner, tags and borrower.
-       *
-       * According to our GraphQL schema, the itemowner and borrower should return
-       * a User (GraphQL type) and tags should return a list of Tags (GraphQL type)
-       *
-       */
-      // @TODO: Uncomment these lines after you define the Item type with these fields
-      async itemowner(parent, args, { pgResource }, info) {
+      async itemowner({ ownerid }, args, { pgResource }, info) {
         try {
-          return await pgResource.getUserById(parent.ownerid);
+          return await pgResource.getUserById(ownerid);
         } catch (e) {
           throw ("itemowner not found");
         }
-        // users.find(user => item.itemowner === user.id);
-        //   // @TODO: Replace this mock return statement with the correct user from Postgres
-        //   return {
-        //     id: 29,
-        //     fullname: "Mock user",
-        //     email: "mock@user.com",
-        //     bio: "Mock user. Remove me."
       },
-      //   // -------------------------------
 
-      async tags(parent, { id }, { pgResource }, info) {
-        // console.log(parent);
-        // console.log(id);
-        //   // @TODO: Replace this mock return statement with the correct tags for the queried Item from Postgres
+      async tags({ id }, args, { pgResource }, info) {
         try {
-          return await pgResource.getTagsForItem(parent.id);
+          return await pgResource.getTagsForItem(id);
         } catch (e) {
           throw ("tags not found");
         }
-        // return pgResource.tags.filter(tag => {
-        //   return tag.id.find(tagId => {
-        //     return pgResource.item_tags.filter(itemTag => {
-        //       return item.id === itemTag.itemid && tagId === itemTag.tagid;
-        //     });
-        //   });
-        // });
-        //   // -------------------------------
       },
-      async borrower(parent, args, { pgResource }, info) {
-        // console.log(parent);
+      async borrower({ borrowerid }, args, { pgResource }, info) {
         try {
-          return await pgResource.getUserById(parent.borrowerid);
+          return await pgResource.getUserById(borrowerid);
         } catch (e) {
           throw ("borrower not found");
         }
-        //   /**
-        //    * @TODO: Replace this mock return statement with the correct user from Postgres
-        //    * or null in the case where the item has not been borrowed.
-        //    */
-        //   return null
-        //   // -------------------------------
-        // return (item.borrower ? pgResource.users.find(user => item.borrower === user.id) : null);
       }
-      // -------------------------------
     },
 
     Mutation: {
@@ -182,34 +104,20 @@ module.exports = app => {
       // ...authMutations(app),
       // -------------------------------
 
-      async addItem(parent, args, context, info) {
-        /**
-         *  @TODO: Destructuring
-         *
-         *  The 'args' and 'context' parameters of this resolver can be destructured
-         *  to make things more readable and avoid duplication.
-         *
-         *  When you're finished with this resolver, destructure all necessary
-         *  parameters in all of your resolver functions.
-         *
-         *  Again, you may look at the user resolver for an example of what
-         *  destructuring should look like.
-         */
-
+      async addItem(parent, { item }, { pgResource }, info) {
         // image = await image;
         // const user = await jwt.decode(context.token, app.get('JWT_SECRET')); 
         try {
           const user = 'verasun'
-          const newItem = await context.pgResource.saveNewItem({
-            item: args.item,
+          const newItem = await pgResource.saveNewItem({
+            item: item,
             image: undefined,
             user
           });
           return newItem;
         } catch (e) {
-          throw new Error("Whoops!");
+          throw ("Whoops!");
         }
-
       }
     }
   };
